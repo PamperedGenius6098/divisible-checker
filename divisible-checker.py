27b5de0767337adf2faed473c9f2efbec581d8c9
Nightmare <56475066+NightmareNinja720@users.noreply.github.com>
2022-10-13 19:44:28 +0530

    Update divisible-checker.py

--- divisible-checker.py
+++ divisible-checker.py
@@ -1,15 +1,15 @@
 t=[]
 p=[]
-r=int(input("enter no. of numbers in tuple:"))
+r=int(input("Enter amount of numbers in tuple:"))
 for i in range(r):
-    x=int(input("enter no."))
+    x=int(input("Enter number"))
     t.append(x)
 for i in range(r):
     a=t[i]%10
     if a==0:
         p.append(t[i])
 t=tuple(t)
-print("tuple =",t)
+print("Tuple =",t)
 p=tuple(p)
-print("divisible by 5 and 2 =",p)
-print("thanks")
+print("Divisible by 5 and 2 =",p)
+print("Thanks for testing!")
